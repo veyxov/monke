590d5ab373d04997f90616b16648a3d25104a008
Shekhov Ismoil <veyxov@gmail.com>
2023-06-04 18:38:59 +0500

    lexer: implement New for the Lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,26 @@
+package lexer
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+
+	l.position = l.readPosition
+	l.readPosition += 1
+}
