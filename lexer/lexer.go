b78113fa96c526ac40e3bce287e930b138a555ec
Shekhov Ismoil <veyxov@gmail.com>
2023-06-04 19:22:06 +0500

    lexer: skip whitespace

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -26,8 +26,16 @@ func (l *Lexer) readIdentifier() string {
 	return l.input[position:l.position]
 }
 
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	l.skipWhitespace()
 
 	switch l.ch {
 	case '=':
